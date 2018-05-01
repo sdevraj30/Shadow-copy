950883c7797eed579ed2ae9838ffb7cb7637cc2c
Devraj Singh <reason2k17@gmail.com>
2018-05-01 20:40:32 +0530

    Source
    This is an executable Arduino code of the project. Here, we extract data from the accelerometer readings, then map these values (250-400) in sync with the range of a servo motor (0-180). The next step is to assign the mapped values to the servo. Lastly, we print all the acccelerometer readings  to better understand and validate the mechanism.

---
+++ Accelerometer.ino
@@ -0,0 +1,31 @@
+#include <Servo.h>
+Servo devaservo;
+int   xAcc = A0;
+int   yAcc = A1;
+int   zAcc = A2;
+int servopin=9;
+void setup(){
+Serial.begin(9600);
+pinMode(xAcc,INPUT);
+pinMode(yAcc,INPUT);
+pinMode(zAcc,INPUT);
+devaservo.attach(servopin);
+}
+void loop()
+{
+int a = analogRead(xAcc);
+int b = analogRead(yAcc);
+int c = analogRead(zAcc);
+a=map(a,250,400,0,180);
+b=map(b,250,400,0,180);
+c=map(c,250,400,0,180);
+devaservo.write(a);
+  Serial.print(" xAcc ");
+  Serial.print(a);
+  Serial.print(" yAcc ");
+  Serial.println(b);
+  Serial.print(" zAcc ");
+  Serial.print(b);
+  Serial.println(" ");
+  delay(250);
+}
